b953860164022d1e9abbd025c10599a7dd9e7513
Jay Anne Bautista <jay-anne@newlogic.com>
2022-08-30 09:53:45 +0800

    chore: initial commit

---
+++ src/index.tsx
@@ -0,0 +1,20 @@
+import { NativeModules, Platform } from 'react-native';
+
+const LINKING_ERROR =
+  `The package 'smartscanner-react-native' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo managed workflow\n';
+
+const SmartscannerReactNative = NativeModules.SmartscannerReactNative  ? NativeModules.SmartscannerReactNative  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+
+export function multiply(a: number, b: number): Promise<number> {
+  return SmartscannerReactNative.multiply(a, b);
+}
